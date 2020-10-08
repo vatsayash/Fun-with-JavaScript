f02e814167d5c33890082026d3e6b0d213b49c06
= <vatsayash@gmail.com>
2020-10-08 22:27:30 +0530

    Varialbes and basic Syntax

---
+++ Src/01_Just_JavaScript.js
@@ -0,0 +1,36 @@
+// console.log() is used to print in Javascript 
+// although it's not javascript it's part of the
+// vast library of API's provided to us
+console.log("Hello JavaScript!");
+
+// javascript is case-sensitive
+// that is a varibale something is different from Something
+
+something = 5;
+Something = 10;
+console.log(something);
+console.log(Something);
+
+
+// Identifiers is a sequnce of characters that can be used to identify a variable, a function,an object
+// It can start with a letter,the dollar $ sign or an underscore _,and it can contain digits.
+// example - test,TEst,test1,_test,$test
+
+// every line is optionally terminated by a semicolons.
+
+// Declaring varibles
+const a = 10;
+// a = 5;  this is not allowed as javascript does not allows reassiging of the variable
+
+let b = 0;
+b = 1;
+
+/*
+const	 	 does	 not	 mean	 "constant"	 in	 the	 way	 some	 other	 languages	 like	 C
+mean.	 In	 particular,	 it	 does	 not	 mean	 the	 value	 cannot	 change	 -	 it	 means	 it
+cannot	be	reassigned.	If	the	variable	points	to	an	object	or	an	array	(we'll	see
+more	about	objects	and	arrays	later)	the	content	of	the	object	or	the	array	can
+freely	change.
+*/
+
+
